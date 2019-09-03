66661dc4cdc697f4a5979a2448f142c2a6f28f93
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2019-09-03 16:17:31 -0300

    acabei o exercício de matrizes

--- aula 010919/matriz.c
+++ aula 010919/matriz.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "matriz.h"
 
 typedef struct matriz{
@@ -13,8 +14,12 @@ Matriz* inicializaMatriz (int nlinhas, int ncolunas){
   mat -> nLinhas = nlinhas;
   mat -> nColunas = ncolunas;
   mat -> mat = (int**) malloc(nlinhas * sizeof(int*));
-  for (int i; i < nlinhas; i++)
-      mat -> mat[i] = (int*) malloc((ncolunas * sizeof(int)));
+  for (int i = 0; i < nlinhas; i++){
+    mat -> mat[i] = (int*) malloc(ncolunas * sizeof(int));
+		for (int j = 0; j < ncolunas; j++){
+			mat -> mat[i][j] = 0;
+		}
+	}
   return mat;
 }
 
@@ -28,43 +33,51 @@ int recuperaElemento(Matriz* mat, int linha, int coluna){
 }
 
 int recuperaNColunas (Matriz* mat){
-	int ncolunas;
-	ncolunas = mat -> nColunas;
+	int ncolunas = mat -> nColunas;
 	return ncolunas;
 }
 
 int recuperaNLinhas (Matriz* mat){
-	int nlinhas;
-	nlinhas = mat -> nLinhas;
+	int nlinhas = mat -> nLinhas;
 	return nlinhas;
 }
 
 Matriz* transposta (Matriz* mat){
 	Matriz* trp;
-	trp = (Matriz*) malloc(sizeof(Matriz));
-  trp -> nLinhas = mat -> nColunas;
-  trp -> nColunas = mat -> nLinhas;
-  trp -> mat = (int**) malloc(trp -> nLinhas * sizeof(int*));
-  for (int i; i < trp -> nLinhas; i++)
-      trp -> mat[i] = (int*) malloc(trp -> nColunas * sizeof(int));
-      trp -> mat[i] = mat -> mat[mat -> nColunas - i];
-  return mat; 
+	trp = inicializaMatriz(mat -> nColunas, mat -> nLinhas);
+	for (int i = 0; i < trp -> nLinhas; i++){
+		for (int j = 0; j < trp -> nColunas; j++){
+			trp -> mat[i][j] = mat -> mat[j][i];
+		}
+	}
+  return trp; 
 }
 
 Matriz* multiplicacao (Matriz* mat1, Matriz* mat2){
-	
+	Matriz *mat = inicializaMatriz(mat1->nLinhas, mat2->nColunas);
+  for (int i = 0; i < mat1->nLinhas; i++) {
+    for (int j = 0; j < mat2->nColunas; j++) {
+      for (int k = 0; k < mat2->nLinhas; k++) {
+        mat -> mat[i][j]+=mat1 -> mat[i][k] * mat2 -> mat[k][j];
+      }
+    }
+  }
+  return mat;
 }
 
 void imprimeMatriz(Matriz* mat){
-	
+	for (int i = 0; i < mat -> nLinhas; i++){
+		for (int j = 0; j < mat -> nColunas; j++){
+			printf("%2d ", mat -> mat[i][j]);
+		}
+		printf("\n");
+	}
 }
 
 void destroiMatriz(Matriz* mat){
-	
+	for (int i = 0; i < mat -> nLinhas; i++){
+    free(mat -> mat[i]);
+  }
+  free(mat -> mat);
+  free(mat);
 }
-
-int main()
-{
-	return 0;
-}
-    
