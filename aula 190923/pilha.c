6368e137b2861a2c5f33fa402f0de17b870d5ff0
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2019-09-23 08:54:30 -0300

    acabei essa pilha antes de acabar a aula!! :relaxed:

--- aula 190923/pilha.c
+++ aula 190923/pilha.c
@@ -1,23 +1,77 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "pilha.h"
 
-#define MAX_PESSOAS 10
+#define MAX_TAM 10
 
-struct pessoa
-{
+struct pessoa {
   char* nome;
   int idade;
   char* endereco;
 };
 
-struct pilha
-{
-  Pessoa pessoas[MAX_PESSOAS];
+struct pilha {
+  Pessoa* pessoas[MAX_TAM];
+  int topo;
 };
 
 Pilha* cria_pilha(){
+  Pilha* pilha = (Pilha*) malloc(sizeof(Pilha));
+  pilha->topo = 0;
   
+  return pilha;
+}
+
+void push(Pessoa* pessoa, Pilha* pilha) {
+  if(pilha->topo == MAX_TAM){
+    return;
+  }
+  pilha->pessoas[pilha->topo] = pessoa;
+  pilha->topo++;
+}
+
+Pessoa* pop(Pilha* pilha) {
+  if(pilha->topo == 0) {
+    return NULL;
+  }
+  Pessoa* pessoa = pilha->pessoas[pilha->topo-1];
+  pilha->topo--;
+  
+  return pessoa;
+}
+
+void imprime_pilha(Pilha* pilha) {
+  for (int i = 0; i < pilha->topo; i++){
+    printf("Nome: %s", pilha->pessoas[i]->nome);
+    printf(" | ");
+    printf("Idade: %d", pilha->pessoas[i]->idade);
+    printf(" | ");
+    printf("Endereco: %s", pilha->pessoas[i]->endereco);
+    printf("\n");
+  }
+  printf("\n");
+}
+
+Pilha* destroi_pilha(Pilha* pilha) {
+  for (int i = 0; i < pilha->topo; i++) {
+    free(pilha->pessoas[i]->endereco);
+    free(pilha->pessoas[i]->nome);
+    free(pilha->pessoas[i]);
+  }
+  free(pilha);
+  return NULL;
+}
+
+Pessoa* inicializaPessoa(char* nome, int idade, char* endereco) {
+  Pessoa* pessoa = (Pessoa*) malloc(sizeof(Pessoa));
+  pessoa->nome = (char*) malloc(strlen(nome));
+  strcpy(pessoa->nome, nome);
+  pessoa->endereco = (char*) malloc(strlen(endereco));
+  strcpy(pessoa->endereco, endereco);
+  pessoa->idade = idade;
+
+  return pessoa;
 }
 
